5a2bd572cdc4b29a7e3cc35d730e0872d524efe2
Alrazihi <tawfikalrazihi@gmail.com>
2021-12-24 05:06:25 -0500

    capture.set(4, height)

--- readimag.py
+++ readimag.py
@@ -1,7 +1,15 @@
 import cv2 as cv
+def rescaleFrame(frame,scale=0.75):
+        width=int(frame.shape[1]*scale)
+        height=int(frame.shape[0]*scale)
+        dimensions =(width,height)
+        return cv.resize(frame, dimensions,interpolation=cv.INTER_AREA)
 
 img =cv.imread('photos/1.png')
 
-cv.imshow('1', img)
+cv.imshow('1', rescaleFrame(img,scale=0.2))
+
+
+
 
 cv.waitKey(0)
--- readvideo.py
+++ readvideo.py
@@ -1,11 +1,24 @@
 import cv2 as cv
 
+
+def rescaleFrame(frame,scale=0.75):
+    #This works for images,videos and live videos
+        width=int(frame.shape[1]*scale)
+        height=int(frame.shape[0]*scale)
+        dimensions =(width,height)
+        return cv.resize(frame, dimensions,interpolation=cv.INTER_AREA)
+
 capture =cv.VideoCapture('videos/1.mp4')
 #0 is your cam VideoCapture(0)
-
+def changeRes(width,height):
+# works only with live videos
+    capture.set(3, width)
+    capture.set(4, height)
 while True:
        isTrue, frame=capture.read()
+       videoresized = rescaleFrame(frame,scale=0.2)
        cv.imshow('Video', frame)
+       cv.imshow('Video Resized', videoresized)
 
        if cv.waitKey(20) & 0xFF==ord('d'):
            #ord(d) means if d is pressed
