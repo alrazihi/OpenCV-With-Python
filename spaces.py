943e05d0058c1294922b4a3f8bd353f49ced4897
Alrazihi <tawfikalrazihi@gmail.com>
2022-01-08 15:50:17 -0500

    spaces

---
+++ spaces.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+img =cv.imread('photos/1.png')
+# cv.imshow('1',img)
+
+# gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow("gray", gray)
+
+# hsv=cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# cv.imshow("hsv", hsv)
+
+# lab=cv.cvtColor(img, cv.COLOR_BGR2LAB)
+# cv.imshow("lab", lab)
+
+rgb=cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow("rgb", rgb)
+plt.imshow(rgb)
+plt.show()
+
+cv.waitKey(0)
